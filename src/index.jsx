9b50dad0d3b25a21b4b5ea2fd5fb1f0c8698d2e3
Nadia Lizcano <priscila.lizcano@gmail.com>
2019-07-05 12:46:49 -0700

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+
+/*eslint-disable */
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App);
+  });
+}
+/*eslint-enable */
