43833ef57eedfbe824a9f29aa03e380918ff45a4
Nadia Lizcano <priscila.lizcano@gmail.com>
2019-07-05 20:52:47 -0700

    Add components folder and App file

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App(){
+  return (
+    <div>
+      <style global jsx>{`
+            
+            `}</style>
+  
+
+    </div>
+  )
+}
+
+export default App
