f991c90b3c1b33853878e32a5c06844853c2e859
Nadia Lizcano <priscila.lizcano@gmail.com>
2019-07-05 22:18:14 -0700

    Add import KegsList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import KegsList from './KegsList'
 
 function App(){
   return (
@@ -6,7 +7,7 @@ function App(){
       <style global jsx>{`
             
             `}</style>
-  
+  <KegsList/>
 
     </div>
   )
