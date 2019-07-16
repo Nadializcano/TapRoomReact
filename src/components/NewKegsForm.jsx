196a636a92c96818dd7159f1cc1607daad61ae70
Nadia Lizcano <priscila.lizcano@gmail.com>
2019-07-16 11:45:57 -0700

    define handleNewKegsSubmission event handler

--- src/components/NewKegsForm.jsx
+++ src/components/NewKegsForm.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 function NewKegsForm(){
+  function handleNewKegsFormSubmission(event){
+    event.preventDefault();
+  }
   return (
     <div>
       <form onSubmit={handleNewKegsFormSubmission}>
