897ce98cbac991f15d3964afd0521fde7860ad6c
Nadia Lizcano <priscila.lizcano@gmail.com>
2019-07-16 09:25:11 -0700

    Declare state

--- src/components/NewKegsControl.jsx
+++ src/components/NewKegsControl.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
 class NewKegsControl extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
   render(){
     return (
       <div>
