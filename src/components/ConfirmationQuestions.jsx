9c83214c1ba2de417278c5ebd7b356b8bf1f8c64
Nadia Lizcano <priscila.lizcano@gmail.com>
2019-07-16 10:24:35 -0700

    Add ConfirmationQuestions component

---
+++ src/components/ConfirmationQuestions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ConfirmationQuestions(){
+  reuturn (
+    <div>
+    <p>Do you want to add a new Keg?</p>
+    <button>Yes</button>
+    </div>
+  );
+}
+
+export default ConfirmationQuestions;
