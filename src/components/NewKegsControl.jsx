09716499eb6a12c5595315e8e9198285bc96d7b3
Nadia Lizcano <priscila.lizcano@gmail.com>
2019-07-16 09:06:40 -0700

    Add NewKegsControl

---
+++ src/components/NewKegsControl.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NewKegsControl extends React.Component {
+  render(){
+    return (
+      <div>
+      <p>test</p>
+      </div>
+    );
+  }
+}
+
+export default NewKegsControl;
